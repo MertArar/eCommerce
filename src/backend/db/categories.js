3f92d64aa612cebf70c34238214a2ec94a58055a
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-30 04:32:35 +0300

    backend db done

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -1,7 +1,21 @@
-import React from "react";
+import { v4 as uuid } from "uuid";
 
-const categories = () => {
-  return <div>categories</div>;
-};
+export const categories = [
+  {
+    _id: "a",
+    categoryName: "Men",
+    img: "/assets/images/category-images/hero-image-men-1.png",
+  },
 
-export default categories;
+  {
+    _id: "b",
+    categoryName: "Women",
+    img: "/assets/images/category-images/hero-image-women-1.png",
+  },
+
+  {
+    _id: "c",
+    categoryName: "Kid",
+    img: "/assets/images/category-images/hero-image-kids-1.png",
+  },
+];
--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -1,7 +1,14 @@
-import React from "react";
+import { v4 as uuid } from "uuid";
+import { formatDate } from "../utils/authUtils";
 
-const users = () => {
-  return <div>users</div>;
-};
-
-export default users;
+export const users = [
+  {
+    _id: uuid(),
+    firstName: "Chirag",
+    lastName: "Taluja",
+    email: "chiragtaluja@apple.com",
+    password: "chiragtaluja",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+];

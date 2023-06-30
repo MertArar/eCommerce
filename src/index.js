d7a16bb0505714735cbcc914b340539d8920fb69
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-30 04:52:01 +0300

    context all done

--- src/index.js
+++ src/index.js
@@ -2,10 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { makeServer } from "./server";
+import { BrowserRouter as Router } from "react-router-dom";
+import { DataProvider } from "./contexts/DataProvider";
+import { AuthProvider } from "./contexts/AuthProvider";
+import { UserProvider } from "./contexts/UserDataProvider";
+import { AddressProvider } from "./contexts/AddressProvider";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+makeServer();
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <AuthProvider>
+        <DataProvider>
+          <UserProvider>
+            <AddressProvider>
+              <App />
+            </AddressProvider>
+          </UserProvider>
+        </DataProvider>
+      </AuthProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );

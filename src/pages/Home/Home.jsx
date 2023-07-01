ac5005ddd52be89c6cea15d23d8ce755c09464e2
MertArar <85193329+MertArar@users.noreply.github.com>
2023-07-01 05:07:28 +0300

    home page added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Footer } from "../../components/Footer/Footer";
+import { HeroSection } from "./components/HeroSection/HeroSection";
+import { CategoriesSection } from "./components/CategoriesSection/CategoriesSection";
+import { VideosSection } from "./components/VideosSection/VideosSection";
+import { HeroVideo } from "./components/HeroVideo/HeroVideo";
+import { useData } from "../../contexts/DataProvider";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { loading } = useData();
+  return (
+    !loading && (
+      <div className="home-page">
+        <div className="hero">
+          <HeroVideo />
+          <HeroSection />
+          <VideosSection />
+          <CategoriesSection />
+          <Footer />
+        </div>
+      </div>
+    )
+  );
 };
 
 export default Home;

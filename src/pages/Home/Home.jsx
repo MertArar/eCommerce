35571d6d6c19e659f361e986d45825d72a6ad128
MertArar <85193329+MertArar@users.noreply.github.com>
2023-07-01 05:17:46 +0300

    bug fixed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Footer } from "../../components/Footer/Footer";
-import { HeroSection } from "./components/HeroSection/HeroSection";
-import { CategoriesSection } from "./components/CategoriesSection/CategoriesSection";
-import { VideosSection } from "./components/VideosSection/VideosSection";
-import { HeroVideo } from "./components/HeroVideo/HeroVideo";
+import Footer from "../../components/Footer/Footer";
+import HeroSection from "./components/HeroSection/HeroSection";
+import CategoriesSection from "./components/CategoriesSection/CategoriesSection";
+import VideosSection from "./components/VideosSection/VideosSection";
+import HeroVideo from "./components/HeroVideo/HeroVideo";
 import { useData } from "../../contexts/DataProvider";
 
 const Home = () => {
--- src/routes/NavRoutes.jsx
+++ src/routes/NavRoutes.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Home from "../pages/Home/Home";
+import Cart from "../pages/Cart/Cart";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const NavRoutes = () => {
-  return <div>NavRoutes</div>;
+export const NavRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
+  );
 };
 
 export default NavRoutes;

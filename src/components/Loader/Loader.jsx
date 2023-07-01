36235c026c4253d34d0b78bdeeb95f128cb8cc41
MertArar <85193329+MertArar@users.noreply.github.com>
2023-07-01 05:02:35 +0300

    loader done

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { SyncLoader } from "react-spinners";
+import { useData } from "../../contexts/DataProvider";
 
 const Loader = () => {
-  return <div>Loader</div>;
+  const { loading } = useData();
+  const override = {
+    position: "absolute",
+    top: "50vh",
+    left: "50vw",
+  };
+  return <SyncLoader cssOverride={override} loading={loading} color="black" />;
 };
 
 export default Loader;

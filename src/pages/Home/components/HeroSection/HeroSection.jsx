60c5f29c6c26869c7740ee0999e6a4699bbc7de9
MertArar <85193329+MertArar@users.noreply.github.com>
2023-07-10 05:35:56 +0300

    heroSection all done

--- src/pages/Home/components/HeroSection/HeroSection.jsx
+++ src/pages/Home/components/HeroSection/HeroSection.jsx
@@ -5,7 +5,7 @@ import "./HeroSection.css";
 const HeroSection = () => {
   return (
     <>
-      <div className="hero-headind-container">
+      <div className="hero-heading-container">
         <h1 className="hero-title">Our Bestsellers</h1>
         <h2 className="hero-subtitle">
           Discover the Heroic Sneaker Line that Transcends Boundries

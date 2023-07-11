b4f66c112612e4f541f325d8085695ac603e8f75
MertArar <85193329+MertArar@users.noreply.github.com>
2023-07-11 05:00:11 +0300

    Logout section all done

--- src/pages/auth/Logout/Logout.jsx
+++ src/pages/auth/Logout/Logout.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Toast, toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../../contexts/AuthProvider";
+import { useUserData } from "../../../contexts/UserDataProvider";
+
+import "./Logout.css";
 
 const Logout = () => {
-  return <div>Logout</div>;
+  const { dispatch } = useUserData();
+  const navigate = useNavigate();
+  const { setAuth } = useAuth();
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isAuth");
+    localStorage.removeItem("token");
+    setAuth({ token: "", isAuth: false });
+    toast.success("You are logged out successfully!");
+    dispatch({ type: "SET_CART", payload: [] });
+    dispatch({ type: "SET_WISHLIST", payload: [] });
+    navigate("/");
+  };
+
+  return (
+    <div className="logout-container">
+      <button onClick={logoutHandler}>Logout</button>
+    </div>
+  );
 };
 
 export default Logout;

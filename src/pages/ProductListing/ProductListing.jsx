a634fb064fcc6a8307d613c43461c05d0d4820ef
MertArar <85193329+MertArar@users.noreply.github.com>
2023-07-12 19:03:45 +0300

    productListing page added

--- src/pages/ProductListing/ProductListing.jsx
+++ src/pages/ProductListing/ProductListing.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Filter from "./components/Filter/Filter";
+import ProductListingSections from "./components/ProductListingSection/ProductListingSection";
+import { useData } from "../../contexts/DataProvider";
 
 const ProductListing = () => {
-  return <div>ProductListing</div>;
+  const { loading } = useData();
+
+  return (
+    !loading && (
+      <div className="page-container">
+        <Filter className="filters" />
+        <ProductListing className="product-container" />
+      </div>
+    )
+  );
 };
 
 export default ProductListing;
